d6afb0d4ef00514333ef148ea0b91eaaa02058f3
Julien Baudru <julien.baudru@gmail.com>
2021-11-18 11:24:42 +0100

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from os.path import exists
+import time
 
 import pytesseract
 from pdf2image import convert_from_path
@@ -14,54 +15,119 @@ import colorama
 
 path = 'tmp.png'
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract'
-flag_show_gui = False
+flag_show_gui = True
 flag_screenshot = False
 flag_timer = False
 flag_show_text = False
+flag_show_image = False
+current_iteration = 0
 
 def main():
+  global current_iteration
   colorama.init()
-  path = handleInput()
-  if(path):
-    showTitle()
+  path, output, repeat = handleInput()
 
-    """
-    # screenshot
+  if(flag_screenshot):
+    path = "tmp.png"
     myScreenshot = pyautogui.screenshot()
     myScreenshot.save(path)
-    """
 
-    txt = pytesseract.image_to_string(path)
-    file = open("output.txt","w")
-    file.write(txt)
-    file.close()
+  if(path):
+    if(flag_show_gui):
+      showTitle()
 
-    blob = TextBlob(txt, analyzer=NaiveBayesAnalyzer())
+    if(flag_timer):
+      if(flag_show_gui):
+        print(" =================================================")
+      start = time.time()
+      now = time.time()
+      while(now - start < repeat):
+        if(int(now-start) % 1 == 0):
+          path = "tmp.png"
+          myScreenshot = pyautogui.screenshot()
+          myScreenshot.save(path)
+
+          if(flag_show_gui):
+            print(" Iteration :", current_iteration)
+          now = time.time()
+          current_iteration += 1
+          analyzeTxt(path, output)
+    else:
+      analyzeTxt(path, output)
+
+def analyzeTxt(path, output):
+  global current_iteration, flag_timer
+  txt = pytesseract.image_to_string(path)
+  if(output):
+    out = output
+  else:
+    if(flag_timer):
+      out = "output" + str(current_iteration) + ".txt"
+    else:
+      out = "output.txt"
+  file = open(out,"w")
+  file.write(txt)
+  file.close()
+  blob = TextBlob(txt, analyzer=NaiveBayesAnalyzer())
+  if(flag_show_gui):
+    if(flag_show_text):
+      showRawTxt(txt)
     showResults(txt, blob)
+    if(flag_show_image):
+      showImage()
 
 def handleInput():
+  global flag_show_gui, flag_screenshot, flag_show_image, flag_show_text, flag_timer
   if(len(sys.argv)==1):
     cprint("Error - Try : python mulder.py -i path/to/file.png", 'red')
     quit()
   else:
     if(len(sys.argv)%2!=1):
       cprint("Error - No value given", 'red')
+      showHelp()
       quit()
     else: # input style : -o name
-      name = "tmp" # TODO : if name is tmp then delete file after
+      name = None # TODO : if name is tmp then delete file after
+      output = None
+      repeat = None
       for i in range(1, len(sys.argv), 2):
         option_i = sys.argv[i]
         argument_i = sys.argv[i+1]
-        print(option_i, argument_i)
-        # option screen shot
         # option screen shot a repetition
-        # option no graphics
-        # option créer
-        if(option_i == "-i"):
+        if(option_i == "-i"): # input
           name = argument_i
-          if(exists(name)):
-            return name
-  return None
+        if(option_i == "-o"): # output
+          output = argument_i
+        if(option_i == "-s"): # screenshot
+          if(argument_i == "y"):
+            flag_screenshot = True
+        if(option_i == "-t"): # timer
+          flag_screenshot = True
+          flag_timer = True
+          repeat = int(argument_i)
+        if(option_i == "-img"): # show image
+          if(argument_i == "y"):
+            flag_show_image = True
+        if(option_i == "-txt"): # show image
+          if(argument_i == "y"):
+            flag_show_text = True
+        if(option_i == "-g"): # gui
+          if(argument_i == "n"):
+            flag_show_gui = False
+  return name, output, repeat
+
+def showHelp():
+  print(" -option arg : Description")
+  print(" =================================================")
+  print(" -i path    : Specify input file path")
+  print(" -o path    : Specify output file path")
+  print(" -g 'y/n'   : Show GUI or not")
+  print(" -txt 'y/n' : Show the raw text on the input image")
+  print(" -img 'y/n' : Show the analyzed input image")
+  print(" -s 'y/n'   : Take screenshot as input")
+  print(" -t x       : Take screenshot for x seconds")
+  print(" =================================================")
+
 
 def showTitle():
   cprint("  ___  ___      _     _                  ___", 'green')
@@ -72,17 +138,21 @@ def showTitle():
   cprint("  \_|  |_/\__,_|_|\__,_|\___|_|         /   \\", 'green')
   cprint('  The screen reader.', 'yellow')
 
+
+def showRawTxt(txt):
+  print(" =================================================")
+  print(txt)
+
+
 def showResults(txt, blob):
   len_txt = len(txt)
   nb_word = len(blob.words)
   nb_sentence = len(blob.sentences)
-
   print(" =================================================")
   print(" Statistics : ")
-  txt = "  - Numbers char. :" + str(len_txt)
-  cprint(txt, 'green')
-  print("  - Numbers words :", nb_word )
-  print("  - Numbers sentences :", nb_sentence)
+  print("  - Numbers char. : ", colored(str(len_txt), 'yellow'))
+  print("  - Numbers words :", colored(str(nb_word), 'yellow'))
+  print("  - Numbers sentences :", colored(str(nb_sentence), 'yellow'))
   print(" =================================================")
 
 
