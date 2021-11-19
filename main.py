1107a06c3c2c69915270b535424c7e2eba89bd89
Julien Baudru <julien.baudru@gmail.com>
2021-11-19 09:34:40 +0100

    option update

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ flag_timer = False
 flag_show_text = False
 flag_show_image = False
 flag_word = False
+flag_sentence = False
 current_iteration = 0
 
 def main():
@@ -32,8 +33,7 @@ def main():
 
   if(flag_screenshot):
     path = "tmp.png"
-    myScreenshot = pyautogui.screenshot()
-    myScreenshot.save(path)
+    takeScreenshot(path)
 
   if(path):
     if(flag_show_gui):
@@ -47,16 +47,22 @@ def main():
       while(now - start < repeat):
         if(int(now-start) % 1 == 0):
           path = "tmp" + str(current_iteration) + ".png"
-          myScreenshot = pyautogui.screenshot()
-          myScreenshot.save(path)
+          takeScreenshot(path)
 
           if(flag_show_gui):
-            print(" New screenshot :", current_iteration, " - ", now-start, "sec.")
+            print(colored(" New screenshot :",'green'), current_iteration, " - ", now-start, "sec.")
+
           now = time.time()
           current_iteration += 1
           analyzeTxt(path, output, nbwords)
     else:
       analyzeTxt(path, output, nbwords)
+
+
+def takeScreenshot(path):
+  myScreenshot = pyautogui.screenshot()
+  myScreenshot.save(path)
+
 
 def analyzeTxt(path, output, nbwords):
   global current_iteration, flag_timer
@@ -79,8 +85,9 @@ def analyzeTxt(path, output, nbwords):
     if(flag_show_image):
       showImage()
 
+
 def handleInput():
-  global flag_show_gui, flag_screenshot, flag_show_image, flag_show_text, flag_timer, flag_word
+  global flag_show_gui, flag_screenshot, flag_show_image, flag_show_text, flag_timer, flag_word, flag_sentence
   if(len(sys.argv)==1):
     cprint(" Error - Try : python mulder.py -i path/to/file.png", 'red')
     quit()
@@ -114,6 +121,9 @@ def handleInput():
             cprint(" Error - Wrong option value", 'red')
             showHelp()
             quit()
+        if(option_i == "-sen"): # most common word
+          if(argument_i == "y"):
+            flag_sentence = True
         if(option_i == "-w"): # most common word
           flag_word = True
           nbwords = int(argument_i)
@@ -130,6 +140,7 @@ def handleInput():
         # CHECK IF A WORD IS IN A PICTURE
   return name, output, repeat, nbwords
 
+
 def showHelp():
   print(" -option arg : Description")
   print(" =================================================")
@@ -137,6 +148,7 @@ def showHelp():
   print(" -o path    : Specify output file path")
   print(" -g 'y/n'   : Show GUI or not")
   print(" -txt 'y/n' : Show the raw text on the input image")
+  print(" -sen 'y/n' : Show the sentences on the input image")
   print(" -img 'y/n' : Show the analyzed input image")
   print(" -s 'y/n'   : Take screenshot as input")
   print(" -t x       : Take screenshot for x seconds")
@@ -168,31 +180,33 @@ def showResults(txt, blob, nbwords):
   counter = Counter(split_it)
   if(flag_word):
     most_commo = counter.most_common(nbwords)
-  else:
-    most_commo = counter.most_common(5)
   print(" =================================================")
   print(" Statistics : ")
   print("  - Numbers char. : ", colored(str(len_txt), 'yellow'))
   print("  - Numbers words :", colored(str(nb_word), 'yellow'))
   print("  - Numbers sentences :", colored(str(nb_sentence), 'yellow'))
   print(" =================================================")
-  print(" Most common words : ")
-  for key, val in most_commo:
-      print("  - ", key, ":", colored(str(val), 'yellow'))
-  print(" =================================================")
-
+  if(flag_word):
+      print(" Most common words : ")
+      for key, val in most_commo:
+          print("  - ", key, ":", colored(str(val), 'yellow'))
+      print(" =================================================")
+  if(flag_sentence):
+      print(" Sentences : ")
+      for elem in blob.sentences:
+          print("  -", elem)
+      print(" =================================================")
 
 def showImage():
   img = cv2.imread(path)
   h, w, _ = img.shape # assumes color image
   boxes = pytesseract.image_to_boxes(img) # also include any config options you use
-  # draw the bounding boxes on the image
   for b in boxes.splitlines():
       b = b.split(' ')
       img = cv2.rectangle(img, (int(b[1]), h - int(b[2])), (int(b[3]), h - int(b[4])), (0, 255, 0), 2)
-  # show annotated image and wait for keypress
   cv2.imshow(path, img)
   cv2.waitKey(0)
+
 
 if __name__ == '__main__':
   main()
