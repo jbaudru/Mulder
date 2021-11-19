81dbbda36adc62e358095487197fa9d671461f70
Julien Baudru <julien.baudru@gmail.com>
2021-11-19 09:21:32 +0100

    new option

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ import cv2
 from textblob import TextBlob
 from textblob.sentiments import NaiveBayesAnalyzer
 import pyautogui
+from collections import Counter
 
 from termcolor import colored, cprint
 import colorama
 
+# TODO : transform image to have a better text extraction (treshold)
 
 path = 'tmp.png'
 pytesseract.pytesseract.tesseract_cmd = r'C:\\Program Files (x86)\\Tesseract-OCR\\tesseract'
@@ -20,12 +22,13 @@ flag_screenshot = False
 flag_timer = False
 flag_show_text = False
 flag_show_image = False
+flag_word = False
 current_iteration = 0
 
 def main():
   global current_iteration
   colorama.init()
-  path, output, repeat = handleInput()
+  path, output, repeat, nbwords = handleInput()
 
   if(flag_screenshot):
     path = "tmp.png"
@@ -43,19 +46,19 @@ def main():
       now = time.time()
       while(now - start < repeat):
         if(int(now-start) % 1 == 0):
-          path = "tmp.png"
+          path = "tmp" + str(current_iteration) + ".png"
           myScreenshot = pyautogui.screenshot()
           myScreenshot.save(path)
 
           if(flag_show_gui):
-            print(" Iteration :", current_iteration)
+            print(" New screenshot :", current_iteration, " - ", now-start, "sec.")
           now = time.time()
           current_iteration += 1
-          analyzeTxt(path, output)
+          analyzeTxt(path, output, nbwords)
     else:
-      analyzeTxt(path, output)
+      analyzeTxt(path, output, nbwords)
 
-def analyzeTxt(path, output):
+def analyzeTxt(path, output, nbwords):
   global current_iteration, flag_timer
   txt = pytesseract.image_to_string(path)
   if(output):
@@ -72,24 +75,25 @@ def analyzeTxt(path, output):
   if(flag_show_gui):
     if(flag_show_text):
       showRawTxt(txt)
-    showResults(txt, blob)
+    showResults(txt, blob, nbwords)
     if(flag_show_image):
       showImage()
 
 def handleInput():
-  global flag_show_gui, flag_screenshot, flag_show_image, flag_show_text, flag_timer
+  global flag_show_gui, flag_screenshot, flag_show_image, flag_show_text, flag_timer, flag_word
   if(len(sys.argv)==1):
-    cprint("Error - Try : python mulder.py -i path/to/file.png", 'red')
+    cprint(" Error - Try : python mulder.py -i path/to/file.png", 'red')
     quit()
   else:
     if(len(sys.argv)%2!=1):
-      cprint("Error - No value given", 'red')
+      cprint(" Error - No value given", 'red')
       showHelp()
       quit()
     else: # input style : -o name
       name = None # TODO : if name is tmp then delete file after
       output = None
       repeat = None
+      nbwords = None
       for i in range(1, len(sys.argv), 2):
         option_i = sys.argv[i]
         argument_i = sys.argv[i+1]
@@ -104,17 +108,27 @@ def handleInput():
         if(option_i == "-t"): # timer
           flag_screenshot = True
           flag_timer = True
-          repeat = int(argument_i)
+          try:
+            repeat = int(argument_i)
+          except:
+            cprint(" Error - Wrong option value", 'red')
+            showHelp()
+            quit()
+        if(option_i == "-w"): # most common word
+          flag_word = True
+          nbwords = int(argument_i)
         if(option_i == "-img"): # show image
           if(argument_i == "y"):
             flag_show_image = True
-        if(option_i == "-txt"): # show image
+        if(option_i == "-txt"): # show txt
           if(argument_i == "y"):
             flag_show_text = True
         if(option_i == "-g"): # gui
           if(argument_i == "n"):
             flag_show_gui = False
-  return name, output, repeat
+
+        # CHECK IF A WORD IS IN A PICTURE
+  return name, output, repeat, nbwords
 
 def showHelp():
   print(" -option arg : Description")
@@ -126,6 +140,7 @@ def showHelp():
   print(" -img 'y/n' : Show the analyzed input image")
   print(" -s 'y/n'   : Take screenshot as input")
   print(" -t x       : Take screenshot for x seconds")
+  print(" -w x       : Show the x most common words")
   print(" =================================================")
 
 
@@ -144,15 +159,26 @@ def showRawTxt(txt):
   print(txt)
 
 
-def showResults(txt, blob):
+# Make function to get Statistics
+def showResults(txt, blob, nbwords):
   len_txt = len(txt)
   nb_word = len(blob.words)
   nb_sentence = len(blob.sentences)
+  split_it = txt.split()
+  counter = Counter(split_it)
+  if(flag_word):
+    most_commo = counter.most_common(nbwords)
+  else:
+    most_commo = counter.most_common(5)
   print(" =================================================")
   print(" Statistics : ")
   print("  - Numbers char. : ", colored(str(len_txt), 'yellow'))
   print("  - Numbers words :", colored(str(nb_word), 'yellow'))
   print("  - Numbers sentences :", colored(str(nb_sentence), 'yellow'))
+  print(" =================================================")
+  print(" Most common words : ")
+  for key, val in most_commo:
+      print("  - ", key, ":", colored(str(val), 'yellow'))
   print(" =================================================")
 
 
